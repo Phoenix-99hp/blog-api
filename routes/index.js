7d29630087e3ccdceec780c5c8caeb5bd9a4b4d4
Phoenix <drake.mcgarrah@gmail.com>
2020-08-16 09:46:33 -0500

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ const authorController = require("../controllers/authorController");
 const cors = require('cors');
 
 const corsOptions = {
-    origin: "https://phoenix-99hp.github.io/blog-posts",
-    optionsSuccessStatus: 200
-}
-
-const corsOptionsAuthor = {
-    origin: "https://phoenix-99hp.github.io/blog-author",
+    origin: "https://phoenix-99hp.github.io",
     optionsSuccessStatus: 200
 }
 
@@ -24,14 +19,14 @@ module.exports = function (app) {
     app.post("/api/posts", cors(corsOptions), postController.newComment);
 
     // Blog author routes
-    app.post("/api/posts/new", cors(corsOptionsAuthor), authorController.newPost);
+    app.post("/api/posts/new", cors(corsOptions), authorController.newPost);
 
-    app.delete("/api/posts/:id/delete", cors(corsOptionsAuthor), authorController.deletePost);
+    app.delete("/api/posts/:id/delete", cors(corsOptions), authorController.deletePost);
 
-    app.post("/api/login", cors(corsOptionsAuthor), authorController.login);
+    app.post("/api/login", cors(corsOptions), authorController.login);
 
-    app.delete("/api/comments/:id/delete", cors(corsOptionsAuthor), authorController.deleteComment);
+    app.delete("/api/comments/:id/delete", cors(corsOptions), authorController.deleteComment);
 
-    app.delete("/api/posts/:id/comments/delete", cors(corsOptionsAuthor), authorController.deleteAllComments);
+    app.delete("/api/posts/:id/comments/delete", cors(corsOptions), authorController.deleteAllComments);
 
 }
